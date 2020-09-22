d699b7cbc7bf0806e92e0f7b2126ed64152acbca
younghun YOO <yyhun92123@naver.com>
2020-09-22 20:23:25 +0900

    Create Customer js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '유영훈',
+  'birthday' : '921222',
+  'gender' : '남',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '960504',
+  'gender' : '남',
+  'job' : '대학생'
+},
+,
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이순신',
+  'birthday' : '970604',
+  'gender' : '남',
+  'job' : '대학생'
+}]
 
 class App extends Component{
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develope management system!</h2>  
+      <div>
+      {
+        customers.map(c=>{
+          return <Customer
+            key={c.id}//map을 사용할때에는 key를 사용해야한다.
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        })
+      }
       </div>
     );
   }
-  
 }
 
 export default App;
